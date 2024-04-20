9aec74a40b6c55201cac85f336018f883fbcfa00
Michał Kalbarczyk <fazibear@gmail.com>
2024-04-20 20:41:47 +0200

    make copy verbose

--- wasm_build.sh
+++ wasm_build.sh
@@ -3,5 +3,5 @@ cargo install wasm-bindgen-cli
 rustup target install wasm32-unknown-unknown
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --no-typescript --target web --out-dir ./out/ ./target/wasm32-unknown-unknown/release/kurvix.wasm
-cp index.html out
-cp -r assets out
+cp -v index.html out
+cp -v -r assets out
