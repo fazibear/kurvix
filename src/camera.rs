4e4b169a3caf49489e9c373aa25a96a8b7986c5f
Michał Kalbarczyk <fazibear@gmail.com>
2024-12-09 16:16:03 +0100

    keep ratio

--- src/camera.rs
+++ src/camera.rs
@@ -3,7 +3,16 @@ use bevy::prelude::*;
 pub struct CameraPlugin;
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d {});
+    commands.spawn((
+        Camera2d {},
+        OrthographicProjection {
+            scaling_mode: bevy::render::camera::ScalingMode::Fixed {
+                width: 1280.,
+                height: 720.,
+            },
+            ..OrthographicProjection::default_2d()
+        },
+    ));
 }
 
 impl Plugin for CameraPlugin {
